bb2769b90da2ceab9122ea64009e0c42407116e5
abdurrahim1122 <abdurrahimdec36@gmail.com>
2025-11-05 13:19:10 +0600

    23-52777-2

---
+++ Calculators/Calculators/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class DoWhileCalculator
+{
+    static void Main()
+    {
+        double num1, num2, result;
+        char operation;
+        char choice;
+
+        do
+        {
+            Console.WriteLine("=== Simple Calculator ===");
+
+            
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter operation (+, -, *, /): ");
+            operation = Convert.ToChar(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+           switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error! Division by zero.");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+
+            Console.Write("\nDo you want to perform another calculation? (y/n): ");
+            choice = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine();
+
+        } while (choice == 'y' || choice == 'Y');
+
+        Console.WriteLine("Thank you for using the calculator!");
+    }
+}
