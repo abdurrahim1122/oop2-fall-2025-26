11c3aba332a06c061597dab20cb6d553da4799ce
abdurrahim1122 <abdurrahimdec36@gmail.com>
2025-11-08 17:18:20 +0600

    23-52777-2

--- Mortgage_Calculator/Mortgage_Calculator/Program.cs
+++ Mortgage_Calculator/Mortgage_Calculator/Program.cs
@@ -10,31 +10,34 @@ class MortgageCalculator
         double monthlyPaymentUSD, monthlyPaymentBDT;
         const double exchangeRate = 120; 
 
+        
+        
+        
         Console.WriteLine("Mortgage Calculator ");
-        Console.Write("Enter the loan amount ($): ");
+        Console.Write("Enter the loan amount: ");
         principal = Convert.ToDouble(Console.ReadLine());
 
        
-        Console.Write("Enter annual interest rate (%): ");
-        annualRate = Convert.ToDouble(Console.ReadLine());
-        monthlyRate = annualRate / 12 / 100;
+     Console.Write("Enter annual interest: ");
+      annualRate = Convert.ToDouble(Console.ReadLine());
+      monthlyRate = annualRate/12/100;
 
       
         Console.Write("Enter the loan term (years): ");
         years = Convert.ToInt32(Console.ReadLine());
-        totalPayments = years * 12;
-        double powerValue = Math.Pow(1 + monthlyRate, totalPayments);
-        monthlyPaymentUSD = principal * (monthlyRate * powerValue) / (powerValue - 1);
-        monthlyPaymentBDT = monthlyPaymentUSD * exchangeRate;
+        totalPayments = years*12;
+        double Value = Math.Pow(1+monthlyRate,totalPayments);
+        monthlyPaymentUSD = principal*(monthlyRate*Value)/(Value -1);
+        monthlyPaymentBDT = monthlyPaymentUSD*exchangeRate;
 
         CultureInfo us = new CultureInfo("en-US");
         Console.WriteLine("\nYour monthly payment:");
-        Console.WriteLine("→ In USD: " + monthlyPaymentUSD.ToString("C2", us));
-        Console.WriteLine("→ In BDT: ৳"+monthlyPaymentBDT.ToString("N2"));
+        Console.WriteLine("USD: " + monthlyPaymentUSD.ToString("C2", us));
+        Console.WriteLine("BDT:"+monthlyPaymentBDT.ToString("N2"));
 
-        Console.WriteLine("\nTotal payment over " + years + " years:");
-        Console.WriteLine("→ In USD: " + (monthlyPaymentUSD * totalPayments).ToString("C2", us));
-        Console.WriteLine("→ In BDT: ৳" + (monthlyPaymentBDT * totalPayments).ToString("N2"));
+        Console.WriteLine("\nTotal payment over "+years+ " years:");
+        Console.WriteLine("USD "+ (monthlyPaymentUSD * totalPayments).ToString("C2", us));
+        Console.WriteLine("BDT:" + (monthlyPaymentBDT * totalPayments).ToString("N2"));
 
         
     }
