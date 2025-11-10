f37a28058fdbd8f36471619cbef9c42639655231
abdurrahim1122 <abdurrahimdec36@gmail.com>
2025-11-10 12:02:06 +0600

    23-52777-2

---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,13 @@
+﻿int a, b, temp;
+Console.Write("Enter first number: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+temp = a;
+a = b;
+b = temp;
+
+Console.WriteLine("\nAfter swapping:");
+Console.WriteLine("a = " +a);
+Console.WriteLine("b =" +b);
